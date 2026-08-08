1c58f9a51ae226c952d59d19570ce2e92753357a
prinzana <prinzana@gmail.com>
2026-08-08 18:34:55 +0100

    feat(optimization): optimized several pages and buttons for mobileRes

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'dev-dist', '*.cjs', '*.js', '**/*.html']),
+  globalIgnores(['dist', 'dev-dist', '*.cjs', '*.js', '**/*.html', 'supabase/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
